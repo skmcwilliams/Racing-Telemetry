f26df765dbcc36ca9df818ec24c5f83f83bafbe0
SKM <skm@Stevens-MacBook-Air.local>
2020-02-13 02:27:06 -0600

    code update

--- RacingData.py
+++ RacingData.py
@@ -2,7 +2,7 @@ import csv
 import pandas as pd
 import numpy as np
 from bokeh.models import Slope, Label
-from bokeh.plotting import figure, show, save, output_file
+from bokeh.plotting import figure, show
 from sklearn.linear_model import LinearRegression
 import traceback
 
@@ -10,15 +10,29 @@ import traceback
 #Open CSV, clean data, renames columns to more widely-used names, takes absolute value of data
 rawdata = pd.read_csv('RayRF9708012014r003.csv', skiprows=18,skip_blank_lines=True)
 lapdata = rawdata.dropna(axis='columns', how='all')
-lapdata.columns = ['Time', 'Distance', 'Brake Pressure', 'Steering Angle', 'Vertical GForce', 'Lateral GForce', 'Longitudinal GForce', 'Gear', 'RPM', 'Throttle%', 'MPH']
+lapdata.columns = ['Time', 'Distance', 'Brake Pressure', 'Steering Angle', 'Vertical GForce', 'Lateral GForce', 'Longitudinal GForce', 'Gear', 'RPM', 'Throttle %', 'MPH']
 lapdata = abs(lapdata)
 
 #Clean new columns so  they can be presented as variables later on
 variables = str([column for column in lapdata.columns])
 
-#Write user prompts to select the variables they wish to compare
-xvariable = input('Type Your Desired X-Axis Variable From ' + variables)
-yvariable = input('Type Your Desired Y-Axis Variable From ' + variables)
+#Create variable counter for function
+variable_counter = 0
+def select_variable():
+    """Write user prompts to select the variables they wish to compare"""
+    global variable_counter
+    if variable_counter < 1:
+        selection = input('Type Your Desired X-Axis Variable From ' + variables)   
+    else:
+        selection = input('Type Your Desired Y-Axis Variable From ' + variables)
+
+    return selection
+
+#Call function for X and Y variables, update counter 
+xvariable = select_variable()
+variable_counter += 1
+yvariable = select_variable()
+variable_counter += 1
 
 def variable_array(variable):
     """convert selected variable to an array for statistics and graphing purposes"""
@@ -63,7 +77,7 @@ p.circle(x,y, size=2, color="blue", legend_label=str(xvariable + ' vs. ' + yvari
 #Plot regression line and legend to plot
 p.line(x,y_predicted, color='orange', legend_label='Regression Line: y='+str(round(slope,2))+'x+'+str(round(intercept,2)))
 
-#Try to save file, print error if not possible
+#Try to show file, print error if not possible
 try:
     #output_file(file_name) - only use if saving file
     show(p)
